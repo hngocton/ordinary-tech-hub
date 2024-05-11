8d49dd25c30c45245dc88c77e725a1d74c869f6d
Ton.Ho <ton.ho@vnhoclt0196.0.1.168.192.in-addr.arpa>
2024-05-11 15:39:53 +0700

    modify personal informations

---
+++ site.config.js
@@ -0,0 +1,74 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "Ton Ho Ngoc",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Engineer & Architect",
+    bio: "Work hard, play harder.",
+    email: "ngocstons.dc@gmail.com",
+    linkedin: "hngocton",
+    github: "hngocton",
+    instagram: "ho.ngocton",
+  },
+  projects: [
+    // {
+    //   name: `morethan-log`,
+    //   href: "https://github.com/morethanmin/morethan-log",
+    // },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "My ordinary home",
+    description: "Where I share things I learned along the way!",
+  },
+
+  // CONFIG configration (required)
+  link: "https://morethan-log.vercel.app",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
